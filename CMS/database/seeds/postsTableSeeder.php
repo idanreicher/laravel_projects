5098b9fa5fd33757cba6bec19235622fb471f07c
idanreicher <idanreicher@gmail.com>
2020-04-20 18:05:46 -0400

    CMS

---
+++ CMS/database/seeds/postsTableSeeder.php
@@ -0,0 +1,102 @@
+<?php
+
+use App\Category;
+use App\Post;
+use App\Tag;
+use Illuminate\Database\Seeder;
+
+class postsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category1 = Category::create([
+
+            'name' => 'News'
+
+         ]);
+
+         $category2 = Category::create([
+
+            'name' => 'Marketing'
+
+         ] );
+
+         $category3 = Category::create([
+
+            'name' => 'Hirimg'
+
+         ] );
+
+        $post1 = Post::create([
+
+            'title' => 'Top 5 brilliant content marketing strategies',
+            'description' => 'TheSaaS is a responsive, professional ',
+            'content' => 'and multipurpose SaaS, Software, Startup and WebApp landing template powered by Bootstrap 4. TheSaaS is a powerful and super flexi',
+            'category_id' => $category2->id,
+            'image' => 'posts/3.jpg'
+
+
+        ]);
+
+        $post2 = Post::create([
+
+            'title' => 'We relocated our office to a new designed garage',
+            'description' => 'TheSaaS is a responsive, professional ',
+            'content' => 'and multipurpose SaaS, Software, Startup and WebApp landing template powered by Bootstrap 4. TheSaaS is a powerful and super flexi',
+            'category_id' => $category3->id,
+            'image' => 'posts/4.jpg'
+
+
+        ]);
+
+        $post3 = Post::create([
+
+            'title' => 'Best practices for minimalist design with example',
+            'description' => 'TheSaaS is a responsive, professional ',
+            'content' => 'and multipurpose SaaS, Software, Startup and WebApp landing template powered by Bootstrap 4. TheSaaS is a powerful and super flexi',
+            'category_id' => $category2->id,
+            'image' => 'posts/1.jpg'
+
+
+        ]);
+
+        $post4 = Post::create([
+
+            'title' => 'New published books to read by a product designer',
+            'description' => 'TheSaaS is a responsive, professional ',
+            'content' => 'and multipurpose SaaS, Software, Startup and WebApp landing template powered by Bootstrap 4. TheSaaS is a powerful and super flexi',
+            'category_id' => $category1->id,
+            'image' => 'posts/2.jpg'
+
+
+        ]);
+
+        $tag1 = Tag::create([
+
+            'name' => 'Design'
+        ]);
+        $tag2 = Tag::create([
+
+            'name' => 'Customers'
+
+        ]);
+        $tag3 = Tag::create([
+
+            'name' => 'Job'
+        ]);
+        $tag4 = Tag::create([
+
+            'name' => 'Screenshot'
+        ]);
+
+
+        $post1->tags()->attach([$tag1->id , $tag3->id]);
+        $post2->tags()->attach([$tag2->id , $tag3->id]);
+        $post3->tags()->attach([$tag1->id , $tag2->id]);
+    }
+}
